9d43d91993c681320110c448b7cadcfea38a842d
jjaniju <152972331+jjaniju@users.noreply.github.com>
2024-02-13 17:09:51 +0200

    viikko 6 tehtävät valmiina!

--- viikko_6/app.js
+++ viikko_6/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const router = require('./routes/index');
 
 var app = express();
 
@@ -18,3 +19,31 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+
+app.get ('/esim',
+function (request, response){
+response.send ('esimerkki');
+console.log ("esimerkki");
+});
+
+
+app.get ('/esim/:nimi',
+function (request, response){
+response.send ('nimi on ' + request.params.nimi);
+console.log ("esimerkki");
+});
+
+app.get ('/esim/:nimi/:sukunimi',
+function (request, response){
+response.send ('nimi on ' + request.params.nimi + " " + request.params.sukunimi);
+console.log ("esimerkki");
+});
+
+app.post ('/',
+function(request,response){
+response.send (request.body.nimi+ ' ' +request.body.sukunimi);
+console.log (request.body.nimi+ ' ' +request.body.sukunimi);
+
+});
+
